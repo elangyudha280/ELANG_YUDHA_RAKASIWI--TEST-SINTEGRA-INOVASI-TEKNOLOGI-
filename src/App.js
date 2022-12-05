7863381954fd7729822ac6dcf49c929f2ff01068
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-05 14:20:09 +0700

    mulai desain component navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import react from 'react';
 function App() {
   return (
     <>
-      <section className="main">
+      <section className="main relative w-full h-[100vh] bg-slate-100">
 
       </section>
     </>
