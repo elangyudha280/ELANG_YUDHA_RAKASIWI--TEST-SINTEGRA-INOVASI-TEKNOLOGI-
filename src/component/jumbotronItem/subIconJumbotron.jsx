bb7491b13ca21298fcff580472b723c4b83df1c2
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-06 14:00:59 +0700

    fix bugs

--- src/component/jumbotronItem/subIconJumbotron.jsx
+++ src/component/jumbotronItem/subIconJumbotron.jsx
@@ -2,11 +2,15 @@
 
 const SubIcon = ({icon,textIcon})=>{
   return (
-    <div className="icon-travel w-[auto] h-[auto] ">
+    <div className="icon-travel w-[auto] h-[80px]">
           <div className="icon w-full rounded-full mx-auto w-[50px] h-[50px] bg-white/70 grid place-items-center cursor-pointer transition-all duration-200 hover:bg-white/100 text-lg  ">
           <i className={icon}></i>
           </div>
-          <p className="text-center text-sm text-white mt-1 font-semibold capitalize">{textIcon}</p>
+          <p className="text-center text-sm text-white mt-1 font-semibold capitalize">
+            {
+            (textIcon === 'land arragement') ? <span>land <br /> arragement</span> : textIcon
+            }
+          </p>
       </div>
   )
 }
@@ -14,12 +18,11 @@ const SubIcon = ({icon,textIcon})=>{
 
 const SubIconJumbotron = ()=>{
     return (
-      <section className="container-icon  mx-auto max-w-md mt-7 flex justify-center gap-2 items-center  flex-wrap">
+      <section className="container-icon  mx-auto max-w-md mt-7 flex justify-center gap-2 items-center  flex-wrap max-[315px]:gap-y-5">
         <SubIcon icon="fa-solid fa-plane-departure text-md text-red-500" textIcon='pesawat' />
-        <SubIcon icon="fa-solid fa-building text-md text-red-500" textIcon='land arragement' />
         <SubIcon icon="fa-solid fa-credit-card text-md text-red-500" textIcon='paylater' />
         <SubIcon icon="fa-solid fa-passport text-md text-red-500" textIcon='visa' />
-
+        <SubIcon icon="fa-solid fa-building text-md text-red-500" textIcon='land arragement' />
         </section>      
     )
 }
