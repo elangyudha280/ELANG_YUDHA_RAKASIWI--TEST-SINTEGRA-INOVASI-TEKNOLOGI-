4215cdfae63fa1087aeb165d14da2beb185aa255
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-06 14:15:58 +0700

    fix icon

--- src/component/jumbotronItem/subIconJumbotron.jsx
+++ src/component/jumbotronItem/subIconJumbotron.jsx
@@ -2,7 +2,7 @@
 
 const SubIcon = ({icon,textIcon})=>{
   return (
-    <div className="icon-travel w-[auto] h-[80px]">
+    <div className="icon-travel w-[90px]   h-[80px]">
           <div className="icon w-full rounded-full mx-auto w-[50px] h-[50px] bg-white/70 grid place-items-center cursor-pointer transition-all duration-200 hover:bg-white/100 text-lg  ">
           <i className={icon}></i>
           </div>
@@ -18,7 +18,7 @@ const SubIcon = ({icon,textIcon})=>{
 
 const SubIconJumbotron = ()=>{
     return (
-      <section className="container-icon  mx-auto max-w-md mt-7 flex justify-center gap-2 items-center  flex-wrap max-[315px]:gap-y-5">
+      <section className="container-icon  mx-auto max-w-md mt-7 flex justify-center  items-center  flex-wrap max-[315px]:gap-y-5">
         <SubIcon icon="fa-solid fa-plane-departure text-md text-red-500" textIcon='pesawat' />
         <SubIcon icon="fa-solid fa-credit-card text-md text-red-500" textIcon='paylater' />
         <SubIcon icon="fa-solid fa-passport text-md text-red-500" textIcon='visa' />
